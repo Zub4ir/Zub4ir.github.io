77a6a5a6c785ca68e80d9675570af7bfd6e673d9
Zubair Patel <36241875+Zub4ir@users.noreply.github.com>
2019-01-30 19:11:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('Hello world!!!');
+
+var firstName = 'Tom';
+console.log(firstName);
+
+var lastName = 'Morrello';
+var age = 27;
+console.log(firstName + ' ' + lastName);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job)
+
+if (age < 30){
+    console.log('less than 30')
+}
